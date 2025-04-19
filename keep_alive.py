fed0a32e4f22abf50b0424b9b095dfb876daaec6
kcji2000 <kimchanj@gmail.com>
2025-04-19 18:20:52 +0900

    bot.py 수정 commit

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,31 @@
+# keep_alive.py
 from flask import Flask
 from threading import Thread
+import requests
+import time
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return "I'm alive!"
 
-def run():
+def run_web():
     app.run(host='0.0.0.0', port=8080)
 
+def self_ping(url, interval=10):
+    """10초마다 자신의 URL을 호출해서 슬립 방지."""
+    while True:
+        try:
+            requests.get(url, timeout=5)
+        except:
+            pass
+        time.sleep(interval)
+
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    # 1) 웹서버 스레드
+    Thread(target=run_web, daemon=True).start()
+    # 2) 자가 ping 스레드
+    url = os.getenv("REPLIT_URL") or "<YOUR_REPLIT_URL>"
+    Thread(target=self_ping, args=(url,), daemon=True).start()
